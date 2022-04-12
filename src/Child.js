c9113579f53c443148f357a0cf59d47b4b01d02c
George-Okumu <gokumu12@gmail.com>
2022-04-12 15:13:07 +0300

    passed data from child to parent

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, childColor }) {
+  function changeMe(){
+    let colorTobePassed = getRandomColor();
+    onChangeColor(colorTobePassed);
+  }
+  return <div className="child" onClick={changeMe} style={{ backgroundColor: childColor }} />;
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,18 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const[childColor, setChildColor] = useState("blue");
+
+  function handleChangeColor(newChildColor){
+    let newColor = getRandomColor();
+    setColor(newColor);
+    setChildColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child childColor={childColor} onChangeColor={handleChangeColor}/>
+      <Child childColor={childColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
